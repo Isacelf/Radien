82050759ddf49aafe3479a0ecfc26bb0d3422c4a
Isac Elfstrand <isac.elfstrand@edu.varberg.se>
2023-10-21 17:38:24 +0200

    Lagt till class, constructor samt funktion.

--- Radien/Program.cs
+++ Radien/Program.cs
@@ -4,19 +4,31 @@
     {
         static void Main(string[] args)
         {
-           
-            
+            Circle cirkelOne = new Circle (5, 3.141f);
+            Circle cirkelTwo = new Circle (6, 3.141f);
+
+            double areaOne = cirkelOne.getArea();
+            double areaTwo = cirkelTwo.getArea();
+
+            Console.WriteLine("The area of the circle with a radius of 5 is: " +areaOne);
+            Console.WriteLine("The area of the circle with a radius of 6 is: " +areaTwo);
 
 
 
-    
-        
         }
-
-
-
-
-
-
+    }
+    class Circle
+    {
+        public float Pi = 3.141f;
+        public int Radius;
+        public Circle(int radius, float pi)
+        {
+            Radius = radius;
+            Pi = pi;
+        }
+        public double getArea()
+        {
+            return Radius * Radius * Pi;
+        }
     }
 }
